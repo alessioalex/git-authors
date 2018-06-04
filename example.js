f2e627854e548f6f7dcfbb913da3fbb570c1ac04
Ken Bingham <w@qrk.us>
2018-06-03 22:04:53 -0400

    append '/.git' if missing from REPO path

--- example.js
+++ example.js
@@ -2,7 +2,8 @@
 
 var gitAuthors = require('./');
 var path = require('path');
-var repoPath = path.resolve(process.env.REPO || (__dirname + '/.git'));
+// append '/.git' if missing from REPO path
+var repoPath = path.resolve(process.env.REPO.replace(/\/(\.git\/?)?$/,"") + '/.git' || (__dirname + '/.git'));
 
 gitAuthors(repoPath, function(err, list) {
   if (err) { throw err; }
